f59ad1475b62d98d6e19c5e03263da41e28cfd38
HimanshuSharma-07 <sharmahimanshu82194@gmail.com>
2025-12-04 01:31:14 +0530

    Implement drag-and-drop functionality and modal toggle for task management

---
+++ script.js
@@ -0,0 +1,74 @@
+const todo = document.querySelector('#todo');
+const progress = document.querySelector('#progress');
+const done = document.querySelector('#done')
+let dragElement = null;
+let count = null;
+
+
+const tasks = document.querySelectorAll('.task');
+
+tasks.forEach(  (task) => {
+    task.addEventListener('drag', (e) => {
+        dragElement = task
+       
+    })
+})  
+
+
+
+
+function addDragsEventsOnColumn(column){
+    column.addEventListener('dragenter', (e) => {
+        e.preventDefault();
+        column.classList.add('hover-over');
+    })
+    column.addEventListener('dragleave', (e) => {
+        e.preventDefault();
+        column.classList.remove('hover-over');
+    })
+
+    column.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    })
+
+    column.addEventListener('drop', (e) => {
+        e.preventDefault();
+        column.appendChild(dragElement)
+        column.classList.remove('hover-over')
+        
+    })
+}
+
+const board = document.querySelectorAll('.board')
+
+board.forEach( (e) => {
+    const columns = e.querySelectorAll('.task-column');
+
+    columns.forEach( (e) => {
+        addDragsEventsOnColumn(e);
+    })
+
+})
+
+const toggelModalButton = document.querySelector("#toggle-modale")
+const modalBg = document.querySelector(".modale .bg")
+const addTaskButton= document.querySelector(".add-new-task")
+const modale= document.querySelector(".modale")
+
+toggelModalButton.addEventListener('click', (e) => {
+    modale.classList.toggle("active")
+    
+})
+
+modalBg.addEventListener('click', (e) => {
+    modale.classList.remove("active")
+    
+})
+
+addTaskButton.addEventListener('click', (e) => {
+        
+    modale.classList.remove("active")
+    
+})
+
+
