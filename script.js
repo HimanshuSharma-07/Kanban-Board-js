f01eb90bdff4a5422320227f026db5c21663ec25
HimanshuSharma-07 <sharmahimanshu82194@gmail.com>
2025-12-06 18:52:53 +0530

    make upadateCount function

--- script.js
+++ script.js
@@ -2,20 +2,32 @@ const todo = document.querySelector('#todo');
 const progress = document.querySelector('#progress');
 const done = document.querySelector('#done')
 let dragElement = null;
-let count = null;
+let count = updateCounts();
 
 
 const tasks = document.querySelectorAll('.task');
 
-tasks.forEach(  (task) => {
+tasks.forEach((task) => {
     task.addEventListener('drag', (e) => {
         dragElement = task
-       
+
     })
-})  
+})
+
+function updateCounts() {
+    const colums = [todo, progress, done]
+    colums.forEach(col => {
+        const tasks = col.querySelectorAll(".task")
+        const count = col.querySelector(".right")
+
+        count.innerText = tasks.length;
+        console.log(count.innerText = tasks.length);
+
+    })
+}
 
 
-function addDragsEventsOnColumn(column){
+function addDragsEventsOnColumn(column) {
     column.addEventListener('dragenter', (e) => {
         e.preventDefault();
         column.classList.add('hover-over');
@@ -31,21 +43,15 @@ function addDragsEventsOnColumn(column){
 
     column.addEventListener('drop', (e) => {
         e.preventDefault();
+
         column.appendChild(dragElement)
         column.classList.remove('hover-over')
 
-         [ todo, progress, done ].forEach( (col) => {
-        const tasks = col.querySelectorAll(".task")
-        const count = col.querySelector(".right")
+        updateCounts()
 
-        count.innerHTML = tasks.length;
     })
 
-        
-   })
- 
-}   
-
+}
 
 addDragsEventsOnColumn(todo)
 addDragsEventsOnColumn(progress)
@@ -54,23 +60,23 @@ addDragsEventsOnColumn(done)
 
 const toggelModalButton = document.querySelector("#toggle-modale")
 const modalBg = document.querySelector(".modale .bg")
-const modale= document.querySelector(".modale")
-const addTaskButton= document.querySelector("#add-new-task-btn")
+const modale = document.querySelector(".modale")
+const addTaskButton = document.querySelector("#add-new-task-btn")
 
 toggelModalButton.addEventListener('click', (e) => {
     modale.classList.toggle("active")
-    
+
 })
 
 modalBg.addEventListener('click', (e) => {
     modale.classList.remove("active")
-    
+
 })
 
 addTaskButton.addEventListener('click', (e) => {
-        
+
     const taskTitle = document.querySelector("#task-input").value
-    const taskDesc = document.querySelector("#task-desc").value  
+    const taskDesc = document.querySelector("#task-desc").value
 
     const div = document.createElement("div")
 
@@ -87,11 +93,10 @@ addTaskButton.addEventListener('click', (e) => {
     })
 
     todo.appendChild(div)
+    updateCounts()
 
     modale.classList.remove("active")
 
-    
-    
 })
 
 
