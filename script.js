331c62dae95de97613d16d313abc298e9a812614
HimanshuSharma-07 <sharmahimanshu82194@gmail.com>
2025-12-07 00:05:53 +0530

    add more features

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
+let tasksData = {}
+
 const todo = document.querySelector('#todo');
 const progress = document.querySelector('#progress');
 const done = document.querySelector('#done')
+const deleteBtb = document.querySelector("button")
 let dragElement = null;
-let count = updateCounts();
+const columns = [todo, progress, done]
+let count = null;
+
+if(localStorage.getItem("tasks")) {
+    
+    const data = JSON.parse(localStorage.getItem("tasks"))
+    
+    for (const col in data) {
+        
+        const column = document.querySelector(`#${col}`)
+        data[col].forEach(task => {
+            
+            const div = document.createElement("div")
+            div.classList.add("task")
+
+            div.setAttribute("draggable", "true")
+
+            div.innerHTML = `
+            <h2>${task.title}</h2>
+            <p>${task.desc}</p>
+            <button>Delete</button>
+            `
+
+            column.appendChild(div)
+
+            div.addEventListener('drag', (e) => {
+                dragElement = div
+            })
+
+        });
+
+        const tasks = column.querySelectorAll(".task")
+        const count = column.querySelector(".right")
+        count.innerText = tasks.length
+        
+    }
+}
 
 
 const tasks = document.querySelectorAll('.task');
@@ -14,17 +53,7 @@ tasks.forEach((task) => {
     })
 })
 
-function updateCounts() {
-    const colums = [todo, progress, done]
-    colums.forEach(col => {
-        const tasks = col.querySelectorAll(".task")
-        const count = col.querySelector(".right")
 
-        count.innerText = tasks.length;
-        console.log(count.innerText = tasks.length);
-
-    })
-}
 
 
 function addDragsEventsOnColumn(column) {
@@ -47,7 +76,24 @@ function addDragsEventsOnColumn(column) {
         column.appendChild(dragElement)
         column.classList.remove('hover-over')
 
-        updateCounts()
+        columns.forEach(col => {
+        const tasks = col.querySelectorAll(".task")
+        const count = col.querySelector(".right")
+
+        tasksData[ col.id ] = Array.from(tasks).map( (t) => {
+            return {
+                title: t.querySelector("h2").innerText,
+                desc: t.querySelector("p").innerText
+            }
+        })
+        
+        localStorage.setItem("tasks", JSON.stringify(tasksData))
+        
+        console.log(tasksData);
+        
+        count.innerText = tasks.length;
+        
+    })
 
     })
 
@@ -88,16 +134,38 @@ addTaskButton.addEventListener('click', (e) => {
         <p>${taskDesc}</p>
         <button>Delete</button>
     `
+    todo.appendChild(div)
+
+
+
     div.addEventListener('drag', () => {
         dragElement = div
     })
 
-    todo.appendChild(div)
-    updateCounts()
+    columns.forEach(col => {
+        const tasks = col.querySelectorAll(".task")
+        const count = col.querySelector(".right")
+
+        tasksData[ col.id ] = Array.from(tasks).map( (t) => {
+            return {
+                title: t.querySelector("h2").innerText,
+                desc: t.querySelector("p").innerText
+            }
+        })
+        
+        localStorage.setItem("tasks", JSON.stringify(tasksData))
+        
+        console.log(tasksData);
+        
+        count.innerText = tasks.length;
+        
+    })
 
     modale.classList.remove("active")
 
 })
+
+
 
 
 
